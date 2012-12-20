2568d1ae199c18fd3fa65f1123137e824c395a28
Laurens Rietveld <laurens.rietveld@vu.nl>
2012-12-20 16:22:15 +0100

    removed number of example bots (need to change/document them for this simplified game engine) use just 1 example bot now (randombot)

--- RandomBot.java
+++ RandomBot.java
@@ -2,28 +2,25 @@ import java.util.*;
 
 public class RandomBot {
 	public static void DoTurn(PlanetWars pw) {
-		// (1) If we current have a fleet in flight, then do nothing until it
-		// arrives.
-		if (pw.MyFleets().size() >= 1) {
-			return;
-		}
-		// (2) Pick one of my planets at random.
+		
+		// (1) Pick one of my planets at random.
 		Random r = new Random();
 		Planet source = null;
 		List<Planet> p = pw.MyPlanets();
 		if (p.size() > 0) {
 			source = p.get(r.nextInt(p.size()));
 		}
-		// (3) Pick a target planet at random.
+		
+		// (2) Pick a target planet at random.
 		Planet dest = null;
 		p = pw.Planets();
 		if (p.size() > 0) {
 			dest = p.get(r.nextInt(p.size()));
 		}
-		// (4) Send half the ships from source to dest.
+		
+		// (3) Send half the ships from source to dest.
 		if (source != null && dest != null) {
-			int numShips = source.NumShips() / 2;
-			pw.IssueOrder(source, dest, numShips);
+			pw.IssueOrder(source, dest);
 		}
 	}
 
