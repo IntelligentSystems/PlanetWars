8e63e0dc111820b86990c6a8aebe165a471a8548
saramagliacane <sara.magliacane@gmail.com>
2012-12-21 13:25:21 +0100

    documenting the RandomBot even more.

--- RandomBot.java
+++ RandomBot.java
@@ -1,29 +1,63 @@
 import java.util.*;
-
+/*
+ RandomBot - an example bot that picks up one of his planets and send half of the ships 
+ from that planet to a random target planet.
+ 
+ Not a very clever bot, but showcases the functions that can be used.
+ Overcommented for educational purposes.
+*/
 public class RandomBot {
+	
+	/* Function that gets called every turn.
+       This is where to implement the strategies. */
+	
 	public static void DoTurn(PlanetWars pw) {
 		
+		// (0) Create a random number generator
+		Random r = new Random(); 
+		
 		// (1) Pick one of my planets at random.
-		Random r = new Random();
+		
 		Planet source = null;
-		List<Planet> p = pw.MyPlanets();
+		
+		// (1a) Take the list of my planets
+		List<Planet> p = pw.MyPlanets(); 
+		
+		
+		// (1b) If the list is not empty:
 		if (p.size() > 0) {
-			source = p.get(r.nextInt(p.size()));
+		
+			// (1c) Pick a random integer in [0, number_of_my_planets] 
+			Integer randomSource = r.nextInt(p.size())
+			
+			// (1d) Pick a random planet as source
+			source = p.get(randomSource);
 		}
 		
-		// (2) Pick a target planet at random.
+		// (2) Pick a target planet at random
 		Planet dest = null;
+		
+		// (2a) Take the list of all planets
 		p = pw.Planets();
+		
 		if (p.size() > 0) {
-			dest = p.get(r.nextInt(p.size()));
+			
+			// (2b) Pick a random integer in [0, number_of_all_planets] 
+			Integer randomTarget = r.nextInt(p.size())
+			
+			// (2c) Pick a random planet as target
+			dest = p.get(randomTarget);
 		}
 		
-		// (3) Send half the ships from source to dest.
+		
+		// (3) Send half the ships from source to destination
 		if (source != null && dest != null) {
 			pw.IssueOrder(source, dest);
 		}
 	}
 
+	
+	
 	public static void main(String[] args) {
 		String line = "";
 		String message = "";
@@ -48,7 +82,7 @@ public class RandomBot {
 				}
 			}
 		} catch (Exception e) {
-			// Owned.
+
 		}
 	}
 }
