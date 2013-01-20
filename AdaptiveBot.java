3c1c45f9561f8dc90456698f9834cb2a0e81e3d7
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2013-01-20 19:14:46 +0100

    Updated adaptivity implementation

--- AdaptiveBot.java
+++ AdaptiveBot.java
@@ -22,19 +22,6 @@ import java.util.*;
 
 public class AdaptiveBot {
 	
-	private static int[] neutralPlanetsKey = {0, 1, 2, 3, 4, 4, 4, 7, 8, 9};
-	private static int[] planetsSizeKey = {3, 3, 3, 3, 3, 3, 3, 3, 3, 3};
-	private static String[] botValue = {"RandomBot", "BullyBot", "BullyBot", "RandomBot", "BullyBot", "BullyBot", "RandomBot", "BullyBot", "BullyBot", "BullyBot"};
-	
-	static {
-		for (int i = 0; i < neutralPlanetsKey.length; i++) {
-			ArrayList<Integer> a = new ArrayList<Integer>();
-			a.add(neutralPlanetsKey[i]);
-			a.add(planetsSizeKey[i]);
-			AdaptivityMap.put(a, "RandomBot");
-		}
-	}
-
 	public static void DoTurn(PlanetWars pw) {
 				
 		//Retrieve environment status
--- AdaptivityMap.java
+++ AdaptivityMap.java
@@ -3,7 +3,18 @@ import java.util.*;
 public class AdaptivityMap {
 	
 	private static HashMap<List<Integer>, String> map = new HashMap<List<Integer>, String>();
+	private static int[] neutralPlanetsKey = {0, 1, 2, 3, 4, 4, 4, 7, 8, 9};
+	private static int[] planetsSizeKey = {3, 3, 3, 3, 3, 3, 3, 3, 3, 3};
+	private static String[] botValue = {"RandomBot", "BullyBot", "BullyBot", "RandomBot", "BullyBot", "BullyBot", "RandomBot", "BullyBot", "BullyBot", "BullyBot"};
 	
+	static {
+		for (int i = 0; i < neutralPlanetsKey.length; i++) {
+			ArrayList<Integer> a = new ArrayList<Integer>();
+			a.add(neutralPlanetsKey[i]);
+			a.add(planetsSizeKey[i]);
+			put(a, botValue[i]);
+		}
+	}
 	
 	public static void put(ArrayList<Integer> keys, String value) {
 		ArrayList<Integer> key = new ArrayList<Integer>();		
