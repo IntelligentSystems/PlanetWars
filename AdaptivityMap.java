480e436389c16140259c8a524b22b3186ccaa200
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2013-01-21 14:40:20 +0100

    Updated comments

--- AdaptivityMap.java
+++ AdaptivityMap.java
@@ -1,8 +1,16 @@
 import java.util.*;
 
 /* An adaptivity map implementation.
- * It maps lists of integers with 
- * 
+ * It maps lists of integers with strings using a HashMap. A HashMap is an implementation of an associative
+ * array (see http://en.wikipedia.org/wiki/Hash_table for further reference).
+ * The list of integers (keys of the hash) represents the environment parameters you want to use for adaptivity. The mapped
+ * strings (values of the hash) represent the bot that performs better under those environment parameters.
+ * In this example, we provide two environment parameters (number of neutral planets, average growth ratio), and we make them
+ * match to a specific bot. The first upper left element of the botValue can be read as: "given that the map has 0 neutral planets
+ * and that the average growth rate of all planets in the map is 0, then user the RandomBot"; the next element to the right: "given
+ * 0 neutral planets and an average growth of 1, use BullyBot; one down the upper left: given 1 neutral planet and average growth
+ * 0, use RandomBot". The given example is random and does not have to be smart at all. We recommend start editing this array to 
+ * get used to adaptivity and discover which other features would make your bot more smartly adaptive. * 
  */
 
 public class AdaptivityMap {
